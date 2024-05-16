60beadc5a19cea8c712dccc1676ae6c8023f191a
xuxy <xuxingyu.cn@qq.com>
2024-05-17 00:07:24 +0800

    Revert "[init] Add default admin"
    This reverts commit a069be88d75d26bdace027351d74acae545c9f26.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux'
 
 import { CSpinner, useColorModes } from '@coreui/react'
 import './scss/style.scss'
-import DefaultAdminLayout from 'src/layout/DefaultAdminLayout'
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
@@ -47,12 +46,6 @@ const App = () => {
           <Route exact path="/register" name="Register Page" element={<Register />} />
           <Route exact path="/404" name="Page 404" element={<Page404 />} />
           <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          <Route
-            exact
-            path="/defaultadmin/*"
-            name="Default Admin"
-            element={<DefaultAdminLayout />}
-          />
           <Route path="*" name="Home" element={<DefaultLayout />} />
         </Routes>
       </Suspense>
